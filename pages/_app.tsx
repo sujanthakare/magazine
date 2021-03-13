dd3026d5e5f325011da0e2375b600416cd34d9de
sujanthakare <sujandt@gmail.com>
2021-03-13 19:02:35 +0100

    Added emotion/react, inline css prop support, typescript support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,29 +13,12 @@ const App: NextPage<AppProps> = ({ Component, pageProps }) => {
         <meta property="og:locale" content={locale} />
         <meta property="og:type" content="website" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="preload" href="/fonts/Ubuntu-Medium.ttf" as="font" crossOrigin="anonymous" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap" rel="stylesheet"></link>
       </Head>
       <Component {...pageProps} />
       <div>
         <h1>Router Locale: {locale}</h1>
         <h1>Default Locale: {defaultLocale}</h1>
       </div>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: 'Fira Sans', sans-serif;
-        }
-
-        * {
-          font-family: 'Fira Sans', sans-serif;
-          box-sizing: border-box;
-        }
-      `}</style>
     </Fragment>
   );
 };
